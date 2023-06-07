66226b525d4e810dfac4dece0179b3def429ab37
hannahf86 <hannahfeehan.design@gmail.com>
2023-06-07 21:17:38 +0100

    first commit

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Landing = () => {
+  
+    const styles = {
+        background: 'absolute w-full h-screen object-cover -z-10',
+        titleSection: 'ml-8 pt-40',
+        title: 'text-white text-7xl my-2',
+        subtitle: 'text-white text-2xl tracking-widest font-thin my-12',
+        buttonSection: 'flex justify-center',
+        button: 'bg-[#303E6E] text-white px-8 py-4 mt-20',
+        description: 'ml-8 text-xl text-white mt-24 mb-16',
+    }
+
+    return (
+    <div id='/'>
+
+      {/* Mobile 
+        <div className='relative'>
+            <img src='src/Assets/background.png' className={styles.background}/>
+
+            <div className={styles.titleSection}>
+                <h1 className={styles.title}>ISAAC</h1>
+                <h1 className={styles.title}>CARDOW</h1>
+                <h2 className={styles.subtitle}>Classical Guitarist</h2>
+            </div>
+
+            <div>
+                <h3 className={styles.description}>A multi-award winning and stylish young guitarist</h3>
+            </div>
+
+            <a href='#' className={styles.buttonSection}>
+                <button className={styles.button}>More info</button>
+            </a>
+        </div> */}
+
+
+      {/* Tablet */}
+
+
+
+      {/* Desktop */}
+
+
+
+    </div>
+  )
+}
+
+export default Landing
